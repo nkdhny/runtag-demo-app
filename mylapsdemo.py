1d035fc768a9ff50645b91f68db45864816b46da
Alexey Golomedov <golomedov@gmail.com>
2015-07-11 20:01:03 +0300

    mobile api

--- mylapsdemo.py
+++ mylapsdemo.py
@@ -1,11 +1,14 @@
+import json
 import logging
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from werkzeug.utils import secure_filename
 from crossdomain import crossdomain
 import os.path
 import cv2
 import runtag.secded
 from flask import send_file
+import base64
+import numpy as np
 
 app = Flask(__name__)
 
@@ -24,11 +27,12 @@ def format_marker(img, m):
 
     return {
         'id': m[0],
-        'left': mx/width,
-        'top': my/height,
-        'width': mw/width,
-        'height': mh/height
+        'left': mx / width,
+        'top': my / height,
+        'width': mw / width,
+        'height': mh / height
     }
+
 
 @app.route('/detect', methods=['POST', 'OPTIONS'])
 @crossdomain(origin='*')
@@ -53,6 +57,7 @@ def upload():
         })
     return "", 500
 
+
 @app.route('/img/<name>', methods=['GET', 'OPTIONS'])
 @crossdomain(origin='*')
 def get_image(name):
@@ -61,6 +66,60 @@ def get_image(name):
         return send_file(local_name)
     else:
         return "", 404
+
+
+@app.route('/m/detect', methods=['POST', 'OPTIONS'])
+@crossdomain(origin='*')
+def detect_mobile():
+    detected = []
+    for shape_serial in json.loads(request.data):
+        shape = decode_shape(shape_serial)
+        m_id = runtag.secded.apply_single(shape)
+
+        if m_id > 0:
+            detected.append(m_id)
+
+    return Response(json.dumps(detected), content_type='application/json')
+
+
+def decode_shape(shape_serial):
+    shape_bytes = bytearray(base64.b64decode(shape_serial))
+
+    s = shape_bytes[::-1]
+
+    print len(shape_serial), len(s), len(base64.b64decode(shape_serial)), shape_serial
+
+    rows_h = s.pop()
+    rows_l = s.pop()
+    rows = rows_h * 256 + rows_l
+
+    cols_h = s.pop()
+    cols_l = s.pop()
+    cols = cols_h * 256 + cols_l
+
+    bit = 0
+
+    portion = 0
+
+    shape = np.zeros((rows, cols), dtype='uint8')
+
+    try:
+        for row in range(0, rows):
+            for col in range(0, cols):
+                if bit % 8 == 0:
+                    portion = s.pop()
+                    bit = 0
+
+                if portion & 0x80 > 0:
+                    shape[row, col] = 255
+
+                portion <<= 1
+
+                bit += 1
+    except Exception as e:
+        print e
+
+    return shape
 
 
 if __name__ == '__main__':
