21572b7c154a47bab818393e1e769e3181bcffaa
Alexey Golomedov <golomedov@gmail.com>
2015-07-12 17:38:58 +0300

    jslint

--- static/app/scripts/directives/upload.js
+++ static/app/scripts/directives/upload.js
@@ -1,3 +1,4 @@
+'use strict';
 angular.module('staticApp')
   .directive('ngFileUpload', function ($log) {
     return {
@@ -25,5 +26,5 @@ angular.module('staticApp')
 			}
 		});
       }
-    }
+    };
 });
